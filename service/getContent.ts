094fe8f3f2afa8bb4d7cc93d5bbca6d94377422e
Max Martens <max.martens@prime-force.com>
2024-08-16 11:40:31 +0200

    check if content is loaded inside an iframe

--- service/getContent.ts
+++ service/getContent.ts
@@ -1,4 +1,19 @@
 export const fetchData = async (path: String) => {
+
+  var isInFrame = null
+  var iframeUrl = null
+
+if (typeof window !== 'undefined') {
+    // Prüfen ob die Seite in einem iFrame geladen wurde
+    isInFrame = window.self !== window.top
+    
+    // URL des iFrames ermitteln
+    iframeUrl = document.referrer || 'Keine URL verfügbar'
+  }
+
+  console.log('isInFrame: ', isInFrame)
+  console.log('iframeUrl: ', iframeUrl)
+
   let url = `${useRuntimeConfig().public.devAuthor}/${
     path.split(':/')[1]
   }.tidy.infinity.json`
