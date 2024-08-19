e7656e7b8d5c14e574d11a0a7df71b6e569abce0
Max Martens <max.martens@prime-force.com>
2024-08-19 10:53:20 +0200

    console.logs

--- server/api/generateToken.ts
+++ server/api/generateToken.ts
@@ -3,27 +3,28 @@ import jwt from 'jsonwebtoken';
 
 export default defineEventHandler(async (event: H3Event) => {
   console.log('GENERATE TOKEN EVENT');
-  const clientId = useRuntimeConfig().public.aemClientId;
-  const imsEndpoint = useRuntimeConfig().public.aemImsEndpoint;
-  const privateKey = useRuntimeConfig().public.aemPrivateKey;
+  // const clientId = useRuntimeConfig().public.aemClientId;
+  // const imsEndpoint = useRuntimeConfig().public.aemImsEndpoint;
+  // const privateKey = useRuntimeConfig().public.aemPrivateKey;
 
-  // Define the payload
-  const payload = {
-    iss: clientId,
-    sub: clientId,
-    aud: `https://${imsEndpoint}/c/${clientId}`,
-    exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60, // 24 hours expiration
-  };
+  // // Define the payload
+  // const payload = {
+  //   iss: clientId,
+  //   sub: clientId,
+  //   aud: `https://${imsEndpoint}/c/${clientId}`,
+  //   exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60, // 24 hours expiration
+  // };
 
-  // Define options for JWT
-  const options = {
-    algorithm: 'RS256',
-    //   expiresIn: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
-  };
+  // // Define options for JWT
+  // const options = {
+  //   algorithm: 'RS256',
+  //   //   expiresIn: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
+  // };
 
-  const token = jwt.sign(payload, privateKey, options);
+  // const token = jwt.sign(payload, privateKey, options);
 
-  return {
-    token,
-  };
+  // return {
+  //   token,
+  // };
+  return 'Token';
 });
