98fb2f5c1dab701770ae5d3ea7e3c3403b7b7d87
Max Martens <max.martens@prime-force.com>
2024-08-19 12:42:53 +0200

    format key

--- server/api/generateToken.ts
+++ server/api/generateToken.ts
@@ -7,6 +7,11 @@ export default defineEventHandler(async (event: H3Event) => {
   const imsEndpoint = useRuntimeConfig().public.aemImsEndpoint;
   const privateKey = useRuntimeConfig().public.aemPrivateKey;
 
+  //format string
+  let formatedKey = privateKey.replace(/\\n/g, '\n');
+
+  formatedKey = formatedKey.replace(/\\r/g, '');
+  // console.log('PrivateKeye: ', privateKeye);
 
   // Define the payload
   const payload = {
@@ -22,7 +27,7 @@ export default defineEventHandler(async (event: H3Event) => {
     //   expiresIn: Math.floor(Date.now() / 1000) + (24 * 60 * 60),
   };
 
-  const token = jwt.sign(payload, privateKey, options);
+  const token = jwt.sign(payload, formatedKey, options);
 
   return {
     token,
