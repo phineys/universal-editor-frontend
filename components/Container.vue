d89d3ba6577076410f61293cc9d7e748e2e8ec26
Max Martens <max.martens@prime-force.com>
2024-08-20 11:58:02 +0200

    wip

--- components/Container.vue
+++ components/Container.vue
@@ -29,8 +29,7 @@ function extractAndRemoveNestedObjects(obj) {
 }
 
 onMounted(async () => {
-  containerData = await fetchData(props.resource);
-  console.log('Container Data: ', fetchData(props.resource));
+  containerData.value = await fetchData(props.resource);
   console.log(containerData.value);
   // nestedObjects.value = extractAndRemoveNestedObjects(containerData.value);
   // console.log(nestedObjects.value);
