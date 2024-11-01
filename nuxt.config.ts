30ba1d56a7e265f451d31869274d0ee1428f821b
Max Martens <max.martens@prime-force.com>
2024-11-01 12:18:53 +0100

    feat: add new method to generate access_token (technical account)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,17 +4,24 @@ import fs from 'fs'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
-    // Keys within public, will be also exposed to the client-side
     public: {
       devAuthor: process.env.DEFAULT_AUTHOR_HOST || 'https://localhost:4502',
       devPublisher: process.env.DEFAULT_PUBLISHER_HOST || 'https://localhost:4502',
-      devCredentials:
-        process.env.DEFAULT_AUTHOR_CREDENTIALS || 'Basic YWRtaW46YWRtaW4=',
-        aemPublicKey: process.env.AEM_PUBLIC_KEY || '',
-        aemPrivateKey: process.env.AEM_PRIVATE_KEY || '',
-        aemClientId: process.env.AEM_CLIENT_ID || '',
-        aemImsEndpoint: process.env.IMS_ENDPOINT || ''
-    }
+      devCredentials: process.env.DEFAULT_AUTHOR_CREDENTIALS || 'Basic YWRtaW46YWRtaW4=',
+      aemPublicKey: process.env.AEM_PUBLIC_KEY || '',
+      aemPrivateKey: process.env.AEM_PRIVATE_KEY || '',
+      aemClientId: process.env.AEM_CLIENT_ID || '',
+      aemImsEndpoint: process.env.IMS_ENDPOINT || '',
+      aem: {
+        clientId: '',
+        technicalAccountId: '',
+        originId: '',
+        clientSecret: '',
+        privateKey: '',
+        metaScopes: '',
+        ims: ''
+      },
+    },
   },
 
   devtools: { enabled: true },
@@ -23,8 +30,8 @@ export default defineNuxtConfig({
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
+      autoprefixer: {},
+    },
   },
 
   app: {
@@ -32,37 +39,37 @@ export default defineNuxtConfig({
       meta: [
         {
           name: 'urn:adobe:aue:system:aemconnection',
-          content: `aem:${process.env.DEFAULT_AUTHOR_HOST}`
+          content: `aem:${process.env.DEFAULT_AUTHOR_HOST}`,
         },
         {
           name: 'urn:adobe:aue:config:service',
-          content: 'https://universal-editor-service.experiencecloud.live'
-        }
+          content: 'https://universal-editor-service.experiencecloud.live',
+        },
       ],
       script: [
         {
-          src: 'https://universal-editor-service.experiencecloud.live/corslib/LATEST'
+          src: 'https://universal-editor-service.experiencecloud.live/corslib/LATEST',
         },
         {
           type: 'application/vnd.adobe.aue.model+json',
-          src: '/model-definition.json'
+          src: '/model-definition.json',
         },
         {
           type: 'application/vnd.adobe.aue.component+json',
-          src: '/component-definition.json'
-        }
-      ]
-    }
+          src: '/component-definition.json',
+        },
+      ],
+    },
   },
 
   devServer: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'server.key')).toString(),
-      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')).toString()
-    }
+      cert: fs.readFileSync(path.resolve(__dirname, 'server.crt')).toString(),
+    },
   },
 
-  modules: ['@nuxt/image', '@nuxtjs/i18n', '@nuxtjs/storybook', "@nuxthub/core"],
+  modules: ['@nuxt/image', '@nuxtjs/i18n', '@nuxtjs/storybook', '@nuxthub/core'],
 
   i18n: {
     lazy: true,
@@ -70,15 +77,15 @@ export default defineNuxtConfig({
     strategy: 'prefix_except_default',
     locales: [
       { code: 'en', iso: 'en-US', name: 'English-US', file: 'en-US.json' },
-      { code: 'de', iso: 'de-DE', name: 'Deutsch', file: 'de-DE.json' }
+      { code: 'de', iso: 'de-DE', name: 'Deutsch', file: 'de-DE.json' },
     ],
     defaultLocale: 'en',
     detectBrowserLanguage: {
       useCookie: true,
       cookieKey: 'i18n_redirected',
-      redirectOn: 'root' // recommended
-    }
+      redirectOn: 'root', // recommended
+    },
   },
 
-  compatibilityDate: '2024-08-19'
-})
+  compatibilityDate: '2024-08-19',
+});
--- server/api/generateToken.ts
+++ server/api/generateToken.ts
@@ -1,40 +1,23 @@
 import { H3Event } from 'h3';
 import * as jose from 'jose';
+  const auth = require('@adobe/jwt-auth');
+  const runtimeConfig = useRuntimeConfig();
 
 export default defineEventHandler(async (event: H3Event) => {
-  console.log('GENERATE TOKEN EVENT');
-  const clientId = useRuntimeConfig().public.aemClientId;
-  const imsEndpoint = useRuntimeConfig().public.aemImsEndpoint;
-  const aemPrivateKey = useRuntimeConfig().public.aemPrivateKey;
 
-  if (!clientId || !imsEndpoint || !aemPrivateKey) {
-    throw new Error('Missing configuration values');
-  }
-
-  const formattedKey = aemPrivateKey.replace(/\\n/g, '\n').replace(/\\r/g, '');
-
-  if (!formattedKey) {
-    throw new Error('Private key is not defined or formatted incorrectly');
-  }
-
-  const payload = {
-    iss: clientId,
-    sub: clientId,
-    aud: `https://${imsEndpoint}/c/${clientId}`,
-    exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
-  };
-  const alg = 'RS256';
-  const privateKey = await jose.importPKCS8(formattedKey, alg);
 
   try {
-    const token = await new jose.SignJWT(payload)
-      .setProtectedHeader({ alg })
-      .setIssuedAt(new Date())
-      .setIssuer(clientId)
-      .setExpirationTime('24h')
-      .sign(privateKey);
+       let tokenResponse = await auth({
+         clientId: runtimeConfig?.public?.aem?.clientId, // Client Id
+         technicalAccountId: runtimeConfig?.public?.aem?.technicalAccountId, // Technical Account Id
+         orgId: runtimeConfig?.public?.aem.originId, // Organization Id
+         clientSecret: runtimeConfig?.public?.aem.clientSecret, // Client Secret
+         privateKey: runtimeConfig?.public?.aem.privateKey, // Private Key
+         metaScopes: runtimeConfig?.public?.aem.metaScopes, // Scopes for the token
+         ims: runtimeConfig?.public?.aem.ims, // IMS
+       });
 
-    return token;
+    return tokenResponse.access_token;
   } catch (error) {
     console.error('Error generating token:', error);
     throw new Error('Token generation failed');
