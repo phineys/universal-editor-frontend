5a75f888a3e7bfc14779eb34ae075474d5cfea23
Max Martens <max.martens@prime-force.com>
2024-11-05 12:47:31 +0100

    do not use try catch

--- server/api/generateToken.ts
+++ server/api/generateToken.ts
@@ -16,7 +16,6 @@ export default defineEventHandler(async (event: H3Event) => {
     ims: runtimeConfig.public.aem.ims,
   };
 
-  // Überprüfen, ob alle erforderlichen Felder vorhanden sind
   for (const [key, value] of Object.entries(credentials)) {
     if (!value) {
       console.error(`Missing required credential: ${key}`);
@@ -24,26 +23,25 @@ export default defineEventHandler(async (event: H3Event) => {
     }
   }
 
-  try {
     let tokenResponse = await auth(credentials);
-    return tokenResponse.access_token;
-  } catch (error) {
-    console.error('Error generating token:', error);
+    console.log('Token generated:', tokenResponse.access_token);
 
-    // Detaillierte Fehlerbehandlung basierend auf dem Fehlerobjekt
-    if ((error as any).response) {
-      // Server antwortete mit einem Statuscode, der außerhalb des Bereichs von 2xx liegt
-      console.error('Response data:', (error as any).response.data);
-      console.error('Response status:', (error as any).response.status);
-      console.error('Response headers:', (error as any).response.headers);
-    } else if ((error as any).request) {
-      // Anfrage wurde gemacht, aber keine Antwort erhalten
-      console.error('Request data:', (error as any).request);
-    } else {
-      // Fehler beim Einrichten der Anfrage
-      console.error('Error message:', (error as any).message);
-    }
+  // try {
+  //   let tokenResponse = await auth(credentials);
+  //   return tokenResponse.access_token;
+  // } catch (error) {
+  //   console.error('Error generating token:', error);
 
-    throw new Error('Token generation failed');
-  }
+  //   if ((error as any).response) {
+  //     console.error('Response data:', (error as any).response.data);
+  //     console.error('Response status:', (error as any).response.status);
+  //     console.error('Response headers:', (error as any).response.headers);
+  //   } else if ((error as any).request) {
+  //     console.error('Request data:', (error as any).request);
+  //   } else {
+  //     console.error('Error message:', (error as any).message);
+  //   }
+
+  //   throw new Error('Token generation failed');
+  // }
 });
