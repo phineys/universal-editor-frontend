42397ef322d40e95c80b9a541b867e95591e767b
Max Martens <max.martens@prime-force.com>
2024-11-05 10:27:55 +0100

    update get-content

--- server/api/get-content.ts
+++ server/api/get-content.ts
@@ -1,3 +1,5 @@
+import auth from '@adobe/jwt-auth';
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { url, isUE } = body;
@@ -17,16 +19,25 @@ export default defineEventHandler(async (event) => {
   const fetchOptions = {};
 
   if (isUE === true) {
-    const token = await $fetch('/api/generateToken').catch((error) => {
-      throw createError({
-        statusCode: 500,
-        statusMessage: `Failed get token from ${url}: ${error.message}`,
+    let tokenResponse;
+    try {
+      tokenResponse = await auth({
+        clientId: runtimeConfig?.public?.aem?.clientId, // Client Id
+        technicalAccountId: runtimeConfig?.public?.aem?.technicalAccountId, // Technical Account Id
+        orgId: runtimeConfig?.public?.aem.originId, // Organization Id
+        clientSecret: runtimeConfig?.public?.aem.clientSecret, // Client Secret
+        privateKey: runtimeConfig?.public?.aem.privateKey, // Private Key
+        metaScopes: runtimeConfig?.public?.aem.metaScopes, // Scopes for the token
+        ims: runtimeConfig?.public?.aem.ims, // IMS
       });
-    });
-    console.log('TOKEN: ', token);
+    } catch (error) {
+      console.error('Error generating token:', error);
+      throw new Error('Token generation failed');
+    }
+    console.log('TOKEN: ', tokenResponse);
 
     fetchOptions.headers = {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${tokenResponse.access_token}`,
       'Cache-Control': 'no-store, no-cache',
       Pragma: 'no-cache',
       Expires: '0',
